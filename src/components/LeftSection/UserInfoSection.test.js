2316542609094b06fc7d1eaf15835a66f36c04b5
HanfordWu <hanfordwu@gmail.com>
2019-11-28 17:14:29 -0500

    test cases

--- src/components/LeftSection/UserInfoSection.test.js
+++ src/components/LeftSection/UserInfoSection.test.js
@@ -2,8 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { UserInfoSection } from './UserInfoSection';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<UserInfoSection />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('Test', function() {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<UserInfoSection />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+
+	it('should be successful', function(){
+		var value = 0;
+		expect(value).to.equal(0);
+	});
+
+	it('should call next() if the authentication succeeds', function(){
+		var request = {
+			isAuthenticated : function() {return true;}
+		},
+			response = {
+				redirect : function(path) { console.log(path);}
+		},
+			next = function(err) {
+				console.log('hi')
+		};
+
+		expect(routes.checkAuthentication(request, response, next)).to.equal(console.log('hi'));
+	});
 });
--- src/components/Message.test.js
+++ src/components/Message.test.js
@@ -2,9 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Messages } from './Messages';
 
-it('renders without crashing', () => {
-  const MessageComponent = Messages;
-  const div = document.createElement('div');
-  ReactDOM.render(MessageComponent, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('Test displayAppropriateSurveyPage', function() {
+
+
+  it('renders without crashing', () => {
+    const MessageComponent = Messages;
+    const div = document.createElement('div');
+    ReactDOM.render(MessageComponent, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+	it('expects rendering same teacher name based on username', function(){
+		var request = {
+			session : {session : {title : "person"}}
+		},
+			response = {
+				render : function(path, obj) {console.log(obj);}
+		},
+			testObj = {
+				userName: 'Haifeng'
+		};
+
+		expect(routes.displayAppropriateSurveyPage(request, response)).to.equal(console.log(testObj));
+	});
+
+	it('expects reponse to be called', function(){
+		var request = {
+			session : {session : {title : "person"}}
+		},
+			response = {
+				render : function(path, obj) {console.log(obj);}
+		};
+
+		routes.displayAppropriateSurveyPage(request, response);
+		expect(response).to.exist;
+		expect(response).to.be.not.empty;
+	});
+
 });
